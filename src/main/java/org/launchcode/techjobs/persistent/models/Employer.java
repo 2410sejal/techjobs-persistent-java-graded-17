f127fd7a4845e7b21cc440b208e5c036260a79c6
techiesandeep <65929239+techiesandeep@users.noreply.github.com>
2024-06-10 12:57:53 -0500

    Task 2 Completed

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,26 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotBlank
+    @Size(min=1, max=50, message = "Must be between 1 to 50 characters")
+    private String location;
+
+    public Employer(String location) {
+        this.location = location;
+    }
+
+    public Employer (){};
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,28 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @NotBlank
+    @Size(min=1, max=255, message = "Must be between 1 to 255 characters")
+    private String description;
+
+    public  Skill(String description) {
+        this.description = description;
+    }
+
+    public Skill (){};
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
 }
